7ebf4acfcfb341e6be68855b46bb638e94f8cdd2
Girard <girard05martin@gmail.com>
2017-11-07 17:49:36 +0100

    Classes Frais(montant) et TypeFrais(libelle, paramCout) + get

---
+++ portailWeb/metier/Frais.js
@@ -0,0 +1,11 @@
+class Frais{
+
+    constructor(montant){
+        this._montant = montant;
+    }
+
+    get montant(){
+        return(this._montant);
+    }
+
+}
---
+++ portailWeb/metier/TypeFrais.js
@@ -0,0 +1,15 @@
+class TypeFrais{
+
+    constructor(libelle, paramCout){
+        this._libelle = libelle;
+        this._paramCout = paramCout;
+    }
+
+    get libelle(){
+        return this._libelle;
+    }
+
+    get paramCout(){
+        return this._paramCout;
+    }
+}
