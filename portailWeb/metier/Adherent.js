4638e46abb442fd99cb7d306a87c7b025514e61d
Bobey <bobey.valentin@gmail.com>
2017-11-07 17:48:16 +0100

    mergons

---
+++ portailWeb/metier/Adherent.js
@@ -0,0 +1,10 @@
+class Adherent
+{
+    constructor()
+    {
+
+    }
+
+}
+
+module.exports = Adherent;
---
+++ portailWeb/metier/Motif.js
@@ -0,0 +1,15 @@
+class Motif
+{
+    constructor(libelle)
+    {
+        this._libelleMotif = libelle;
+    }
+
+    get libelleMotif()
+    {
+        return this._libelleMotif;
+    }
+
+}
+
+module.exports = Motif;
