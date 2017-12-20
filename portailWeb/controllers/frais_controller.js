1b68bfe351d511e296d683b773cfa0f3137cb4a3
audriclemeitour <audriclemeitour@gmail.com>
2017-12-20 13:04:35 +0100

    Ajout des classes Trajet et Ville

--- portailWeb/controllers/frais_controller.js
+++ portailWeb/controllers/frais_controller.js
@@ -1,30 +1,38 @@
+const Frais_DAO = require('../DAO/Frais_DAO');
+const frais_DAO = new Frais_DAO();
+
 //FRAIS
-exports.frais = function(req, res){
-    res.render('frais');
-};
+    exports.frais = function(req, res) {
+        res.render('frais')
+    }
 
 //VERIFICATION
 
-exports.verification = function(req, res)
-{
-    let cout;
-    let kiloM = req.body.parcourus;
-    if (kiloM <= 5000)
-        cout = kiloM * 0.493;
-    else if (kiloM > 5000 && kiloM <= 20000)
-        cout = kiloM * 0.277 + 1082;
-    else
-        cout = kiloM * 0.332;
-    res.render('verification', {motif : req.body.motif, villeD : req.body.villeD,
-        villeA: req.body.villeA, kiloM : req.body.parcourus, cout : cout});
-}
+    exports.verification = function (req, res) {
+        let cout;
+        let kiloM = req.body.parcourus;
+        if (kiloM <= 5000)
+            cout = kiloM * 0.493;
+        else if (kiloM > 5000 && kiloM <= 20000)
+            cout = kiloM * 0.277 + 1082;
+        else
+            cout = kiloM * 0.332;
+        res.render('verification', {
+            motif: req.body.motif, villeD: req.body.villeD,
+            villeA: req.body.villeA, kiloM: req.body.parcourus, cout: cout,
+            date : req.body.date
+        });
+    }
 
 //FRAIS ANNEXE
-exports.fraisAnnexes = function(req, res){
-    res.render('fraisAnnexes');
-};
+    exports.fraisAnnexes = function (req, res) {
+        res.render('fraisAnnexes', {M : req.body.M, vD : req.body.vD, vA : req.body.vA,
+        kl: req.body.kl, c: req.body.c, date : req.body.date});
+    };
 
-exports.fraisIndex = function(req, res)
-{
-    res.redirect('/');
-}
+    exports.fraisIndex = function (req, res) {
+        frais_DAO.addNewDeplacements(req.body.M, req.user.id, req.body.date, req.body.vD,
+            (req.body.vA), (req.body.c), req.body.montantHebergement, req.body.montantRepas,
+            req.body.montantHebergement, (req.body.kl));
+        res.redirect('/');
+    };
--- portailWeb/routes/index.js
+++ portailWeb/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var html_controller = require('../controllers/html_controller');
 
 /* GET home page. */
 router.get('/',
@@ -10,5 +11,22 @@ router.get('/',
         res.render('index', { title: 'Portail de la M2L', user: req.user});
     }
 );
+
+router.get('/hauts_faits', html_controller.hauts_faits);
+
+router.get('/hauts_faits/j_o_p',html_controller.j_o_p);
+
+router.get('/hauts_faits/articles',html_controller.articles);
+
+router.get('/hauts_faits/statistiques',html_controller.statistiques);
+
+router.get('/informations',html_controller.informations);
+
+router.get('/informations/presentation',html_controller.presentation);
+
+router.get('/informations/annuaire',html_controller.annuaire);
+
+
+
 
 module.exports = router;
